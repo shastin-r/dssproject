dbfcc04b4768cd290b03b7f7cd9362c0e4014701
shastin-r <romjustal@gmail.com>
2024-03-22 15:02:52 -0600

    update addsucursal
    adición de php para agregar sucursal, y update de ver movimientos

---
+++ php/addsucursal.php
@@ -0,0 +1,28 @@
+<?php
+
+// Capturar valores del formulario
+$direccion = $_POST['numero2'];
+$telefono = $_POST['numero3'];
+
+// Conectar a la base de datos
+$conn = mysqli_connect("localhost", "root", "", "dssproyecto");
+
+// Preparar la consulta INSERT
+$sql = "INSERT INTO sucursales (Direccion, NumTelefono) VALUES (?, ?, ?)";
+$stmt = mysqli_prepare($conn, $sql);
+
+// Vincular valores a la consulta
+mysqli_stmt_bind_param($stmt, "sss", $nombre, $direccion, $telefono);
+
+// Ejecutar la consulta
+if (mysqli_stmt_execute($stmt)) {
+  echo "Sucursal agregada exitosamente!";
+} else {
+  echo "Error al agregar la sucursal: " . mysqli_stmt_error($stmt);
+}
+
+// Cerrar la conexión
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+
+?>
